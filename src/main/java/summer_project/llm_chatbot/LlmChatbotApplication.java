197967522dadc605bd1cadc08fa52f2ec599b007
yejoo0310 <yejoo0310@naver.com>
2025-07-07 18:07:03 +0900

    init

---
+++ src/main/java/summer_project/llm_chatbot/LlmChatbotApplication.java
@@ -0,0 +1,13 @@
+package summer_project.llm_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmChatbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/summer_project/llm_chatbot/LlmChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package summer_project.llm_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
