ffda9b52991770b7c8d702975f81263b035b8a30
yejoo0310 <yejoo0310@naver.com>
2025-07-15 12:21:40 +0900

    feat: ApplicationException 사용법 변경

--- src/main/java/summer_project/llm_chatbot/error/ApplicationException.java
+++ src/main/java/summer_project/llm_chatbot/error/ApplicationException.java
@@ -1,18 +1,34 @@
 package summer_project.llm_chatbot.error;
 
+import lombok.Getter;
+
+@Getter
 public class ApplicationException extends RuntimeException{
     private final int status;
+    private final String message;
+
+    protected ApplicationException(int status) {
+        this.status = status;
+        this.message = "";
+    }
 
     protected ApplicationException(String message, int status) {
         super(message);
         this.status = status;
+        this.message = message;
     }
 
-    public int getStatus() {
-        return status;
+    protected ApplicationException(String message, int status, Throwable cause, int status1) {
+        super(message, cause);
+        this.status = status1;
+        this.message = message;
     }
 
     public static ApplicationException of(String message, int status) {
         return new ApplicationException(message, status);
+    }
+
+    public static ApplicationException of(int status) {
+        return new ApplicationException(status);
     }
 }
